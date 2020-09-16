1632f134d0a64a7ad0ac5c2de49689cfe41cab2f
Victor Ragojos <vhsvragojos@gmail.com>
2020-09-15 17:49:42 -0700

    MOD: cleanup

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
-export { default as common } from "common";
-
 export { default as QueryBuilder } from "components/QueryBuilder";
 export { default as ResultTable } from "components/ResultTable";
 export { default as Calendar } from "components/Calendar";
