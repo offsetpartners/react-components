1f202603abc00be46097763cf6662ac2f805d041
Victor Ragojos <vhsvragojos@gmail.com>
2020-09-17 00:00:37 -0700

    MOD: Calendar
    - add constants file
    - use new Subcomponents

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,44 +1,99 @@
-import React from "react";
-import "./styles/index.scss";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import { Col, Row, Typography } from "antd";
+import moment from "moment";
+import "./styles/index.less";
+import Week from "./components/Week";
+import Header from "./components/Header";
+import DaysLabel from "./components/DaysLabel";
+import React, { useState, useEffect, memo, useCallback } from "react";
 
-export default function Calendar() {
+export default memo(function Calendar({
+  month,
+  setMonth,
+  year,
+  setYear,
+  render,
+  onDateChange,
+}) {
+  const [mounted, setMounted] = useState(false);
+
+  // Make sure that appropriate Setter Functions are provided
+  // if Calendar is a Controlled Component
+  if ((month && !setMonth) || (year && !setYear)) {
+    throw new Error("You must provide a setter fn for a Controlled Component!");
+  }
+
+  // Default State Setter
+  const [_month, _setMonth] = useState(moment().month());
+  const [_year, _setYear] = useState(moment().year());
+
+  const gatedSetYear = useCallback((v) => {
+    if (setYear && typeof setYear === "function") return setYear(v);
+
+    _setYear(v);
+  }, []);
+
+  const gatedSetMonth = useCallback((v) => {
+    let _v = v;
+    if (v < 0) {
+      _v = 11;
+      gatedSetYear((y) => y - 1);
+    } else if (v > 11) {
+      _v = 0;
+      gatedSetYear((y) => y + 1);
+    }
+
+    if (setMonth && typeof setMonth === "function") return setMonth(_v);
+
+    _setMonth(_v);
+  }, []);
+
+  // Run onDateChange callback
+  useEffect(() => {
+    if (mounted && onDateChange && typeof onDateChange === "function")
+      onDateChange(month || _month, year || _year);
+  }, [month ?? _month, year ?? _year]);
+
+  // Run on Mount
+  useEffect(() => {
+    setMounted(true);
+
+    return () => setMounted(false);
+  }, []);
+
   return (
-    <FullCalendar
-      eventDisplay="none"
-      plugins={[dayGridPlugin]}
-      initialView="dayGridMonth"
-      dayCellClassNames="fig-calendar-day-cell"
-      dayCellContent={(
-        date,
-        _,
-        isPast,
-        isFuture,
-        isToday,
-        isOther,
-        resource,
-        el
-      ) => {
-        const { dayNumberText } = date;
-        return (
-          <Row align="middle" justify="center">
-            <Col span={24}>
-              <Typography.Text>{dayNumberText}</Typography.Text>
-            </Col>
-            <Col span={24}>
-              <Typography.Text>Event</Typography.Text>
-            </Col>
-          </Row>
-        );
-      }}
-      dayHeaderFormat={{ weekday: "narrow" }}
-      headerToolbar={{
-        left: "prevYear,prev",
-        center: "title",
-        right: "next,nextYear",
-      }}
-    />
+    <div className="fig-calendar-wrapper">
+      <table className="fig-calendar">
+        <thead className="fig-calendar-header">
+          <tr className="fig-calendar-header-row">
+            <th className="fig-calendar-header-cell">
+              <Header
+                year={year ?? _year}
+                setYear={gatedSetYear}
+                month={month ?? _month}
+                setMonth={gatedSetMonth}
+              />
+            </th>
+          </tr>
+        </thead>
+
+        <tbody className="fig-calendar-body">
+          <tr className="fig-calendar-container">
+            <DaysLabel />
+          </tr>
+
+          <tr className="fig-calendar-container">
+            {/* Allow for a custom Render fn */}
+            {render && typeof render === "function" ? (
+              render(month || _month, year || _year)
+            ) : (
+              <Week
+                year={year || _year}
+                month={month || _month}
+                onClick={(date) => console.log(date)}
+              />
+            )}
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
-}
+});
---
+++ src/components/Calendar/lib/constants.js
@@ -0,0 +1,50 @@
+export const MONTHS = [
+  { code: "JAN", displayName: "January" },
+  { code: "FEB", displayName: "February" },
+  { code: "MAR", displayName: "March" },
+  { code: "APR", displayName: "April" },
+  { code: "MAY", displayName: "May" },
+  { code: "JUN", displayName: "June" },
+  { code: "JUL", displayName: "July" },
+  { code: "AUG", displayName: "August" },
+  { code: "SEP", displayName: "September" },
+  { code: "OCT", displayName: "October" },
+  { code: "NOV", displayName: "November" },
+  { code: "DEC", displayName: "December" },
+];
+
+export const WEEKS_ARRAY = [0, 1, 2, 3, 4, 5];
+export const DAYS_ARRAY = [0, 1, 2, 3, 4, 5, 6];
+
+export const MIN_YEAR = 1970;
+
+/**
+ * @returns {Array<number>} YEARS
+ */
+export const YEARS_ARRAY = () => {
+  const year = new Date().getFullYear();
+  const len = year - MIN_YEAR + 1;
+  return Array.from(new Array(len), (val, index) => MIN_YEAR + index);
+};
+
+/**
+ * @param {"long"|"short"|"narrow"} type
+ */
+export const GET_WEEKDAYS = (type = "short") => {
+  switch (type) {
+    case "long":
+      return [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+    case "narrow":
+      return ["S", "M", "T", "W", "T", "F", "S"];
+    default:
+      return ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  }
+};
