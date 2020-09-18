7ff48b8cfed178a5b5a77b07e045ec743e8fb71e
Victor Ragojos <vhsvragojos@gmail.com>
2020-09-18 00:02:28 -0700

    MOD: Global style tweak

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
+import "styles/index.less";
+
 export { default as QueryBuilder } from "components/QueryBuilder";
 export { default as ResultTable } from "components/ResultTable";
 export { default as Calendar } from "components/Calendar";
