4886b439172a553913ad90cf61189d7f634b61db
Victor Ragojos <vhsvragojos@gmail.com>
2020-09-23 00:23:17 -0700

    MOD: define FigureReact object for props

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// If FigureReact isn't defined then default to an empty object
+const FigureReact = FigureReact || {};
+
 export { default as common } from "./common";
 
 export * from "./components";
