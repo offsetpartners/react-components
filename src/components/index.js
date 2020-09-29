ac8799b9366608702fa63cea260dfb51c1056fbb
Victor Ragojos <vhsvragojos@gmail.com>
2020-09-29 11:37:31 -0700

    MOD: components exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
-import "styles/index.less";
-
-export { default as QueryBuilder } from "components/QueryBuilder";
-export { default as ResultTable } from "components/ResultTable";
-export { default as Calendar } from "components/Calendar";
+export { default as QueryBuilder } from "./QueryBuilder";
+export { default as ResultTable } from "./ResultTable";
+export { default as Calendar } from "./Calendar";
+export { default as IconText } from "./IconText";
