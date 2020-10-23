fb29808b18e2d2089451e0dc289bf0db4ef912f2
Victor Ragojos <vhsvragojos@gmail.com>
2020-10-23 12:10:59 -0700

    MOD: QueryBuilder
    - allow props passing via HTML

--- src/components/QueryBuilder/QueryBuilder.js
+++ src/components/QueryBuilder/QueryBuilder.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import propTypes from "./propTypes";
 import React, { memo } from "react";
 import { Tabs, Layout } from "antd";
 import QueryTab from "./components/QueryTab";
@@ -36,47 +36,7 @@ const Content = () => {
   );
 };
 
-QueryBuilder.propTypes = {
-  /**
-   * A callback function that will be executed once Query
-   * @typedef {Function|Null} onSave
-   * @param {"orders"|"customers"} type Type of Query <br/>
-   * @param {Array} query Converted Rules to Elastic Search Query <br/>
-   * @param {Function} setRule Function that will re-render the Component as well as
-   * change the Result internal State for the Component. When called, you must pass
-   * an array for function to execute. <br />
-   */
-  onSave: PropTypes.func,
-
-  /**
-   * Sets the ability for Users to change Tabs, Segments/Rules, and Save Button
-   */
-  disabled: PropTypes.bool,
-
-  /**
-   * Inputs
-   * @typedef {{orders: QueryBuilderInput, customers: QueryBuilderInput}} inputs
-   */
-  inputs: PropTypes.shape({
-    orders: PropTypes.object,
-    customers: PropTypes.object,
-  }),
-
-  /**
-   * Initial State of Component
-   * @property {Types} type
-   * @property {Object} query
-   * @property {{orders: Array, customers: Array}} result
-   */
-  initial: PropTypes.shape({
-    type: PropTypes.string,
-    query: PropTypes.object,
-    result: PropTypes.shape({
-      orders: PropTypes.array,
-      customers: PropTypes.array,
-    }),
-  }),
-};
+QueryBuilder.propTypes = propTypes;
 
 QueryBuilder.defaultProps = {
   disabled: false,
--- src/components/QueryBuilder/index.js
+++ src/components/QueryBuilder/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./styles/index.css";
 import { render } from "react-dom";
+import propTypes from "./propTypes";
 import { default as QueryBuilder } from "./QueryBuilder";
 
 // Allows for Component to mount via a normal DOM Classname
 // Similar to jQuery
 const domElement = document.getElementById("fig-query-builder");
 if (domElement) {
+  let providerProps;
+  const validProps = Object.keys(propTypes);
+  if (typeof FigureReact !== "undefined" && FigureReact.QueryBuilder) {
+    providedProps = FigureReact.QueryBuilder;
+  }
+
+  let queryBuilderProps = {};
+
+  try {
+    validProps.forEach((prop) => {
+      queryBuilderProps[prop] = providerProps[prop];
+    });
+  } catch (e) {}
+
   if (domElement instanceof Element) {
     render(<QueryBuilder />, domElement);
   }
