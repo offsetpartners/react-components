65574c4cf12dacf0a118d0c3854aa1c2bbd2330c
Victor Ragojos <vhsvragojos@gmail.com>
2020-11-20 12:38:25 -0800

    MOD: expose FigureReact to window

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 // If FigureReact isn't defined then default to an empty object
-const FigureReact = FigureReact || {};
+if (typeof FigureReact === "undefined") {
+  window.FigureReact = {};
+}
+
+import common from "./common";
+import * as Components from "./components";
+
+FigureReact.common = common;
+FigureReact.Components = Components;
 
 export { default as common } from "./common";
-
 export * from "./components";
