eb1b1939dcecdb0f142349932373507294c6dd84
Victor Ragojos <vhsvragojos@gmail.com>
2021-02-04 11:51:07 -0800

    MOD: cleanup

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as Filter } from "./Filter";
 export { default as DatePicker } from "./DatePicker";
 export { default as Calendar } from "./Calendar";
-export { default as IconText } from "./IconText";
 export { default as TimePicker } from "./TimePicker";
 export { default as SearchSelect } from "./SearchSelect";
